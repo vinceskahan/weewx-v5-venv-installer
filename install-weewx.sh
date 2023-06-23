b7b6d10a06ec9b6289313d6fb16a66d6a1fb1a49
Vince Skahan <vinceskahan@gmail.com>
2023-06-23 15:21:38 -0700

    initial os family detection

--- install-weewx.sh
+++ install-weewx.sh
@@ -6,6 +6,27 @@
 #        which is a bash builtin, so this
 #        should be invoked as "bash scriptname"
 #----------------------------------------------------
+#
+# TESTED IN VAGRANT ON:
+#    debian12
+#    ubuntu2204
+#    rocky9
+#    almalinux9
+#
+# can't test centos/9 - no official vagrant boxes released
+# can't test rhel/9   - no official vagrant boxes released
+# can't test fedora   - no official vagrant boxes released
+#
+#
+# TESTING NOTE:
+#   running 'apt-get upgrade -y' can sometimes still pop a
+#   dialog box causing this script to hang, asking what to
+#   do with pre-existing os config files.  This was seen in
+#   updating the ssh package(s) to current while using an
+#   old vagrant base box.   It would be likely wiser to
+#   do the os package updates outside this script....
+#
+
 
 LOGFILE=${HOME}/install-weewx.log
 
@@ -18,7 +39,7 @@ function run_command() {
     retval=$?
     if [ "x${retval}" = "x0" ]
     then
-	echo "   ok"
+    echo "   ok"
     else 
         echo "#  failed - error ${retval}" >> $LOGFILE
         echo "   failed - error ${retval}"
@@ -26,6 +47,23 @@ function run_command() {
         # TO DO - this should abort out gracefully (probably)
     fi
     echo "#"                                      >> $LOGFILE
+}
+
+function upgrade_apt() {
+    echo "updating apt"
+    run_command "sudo apt-get update"
+    echo "upgrading dpkg packages"
+    run_command "sudo apt-get upgrade -y"
+}
+
+function upgrade_rpm() {
+    echo "upgrading rpm packages"
+    run_command "sudo yum update -y"
+}
+
+function os_prerequisites_rpm() {
+    echo "installing pip"
+    run_command "sudo yum install -y python3-pip"
 }
 
 echo "#-----------------------------------------" >  $LOGFILE
@@ -36,14 +74,32 @@ echo ""                                           >> $LOGFILE
 
 echo "installing weewx..."
 
-echo "updating apt"
-run_command "sudo apt-get update"
-
-echo "upgrading packages"
-run_command "sudo apt-get upgrade -y"
-
-echo "installing python venv modules"
-run_command "sudo apt-get install -y python3-pip python3-venv"
+echo "detecting os family and upgrading os packages"
+if [ -f /etc/os-release ]
+then
+    . /etc/os-release
+fi
+case ${ID} in
+debian)    upgrade_apt; 
+           run_command "sudo apt-get install -y python3-pip python3-venv";
+           ;;
+ubuntu)    upgrade_apt;
+           run_command "sudo apt-get install -y python3-pip python3-venv";
+           ;;
+rocky)     upgrade_rpm;
+           run_command "sudo yum install -y python3-pip";
+           run_command "sudo pip3 install virtualenv"
+           ;;
+almalinux) upgrade_rpm;
+           run_command "sudo yum install -y python3-pip";
+           run_command "sudo pip3 install virtualenv"
+           ;;
+fedora)    upgrade_rpm;
+           run_command "sudo yum install -y python3-pip";
+           run_command "sudo pip3 install virtualenv"
+           ;;
+*)         echo "unknown os family - exiting" ; exit 1;;
+esac
 
 echo "creating python venv"
 run_command "python3 -m venv weewx-data"
@@ -68,6 +124,13 @@ run_command "sudo systemctl daemon-reload"
 
 echo "enable weewx"
 run_command "sudo systemctl enable weewx"
+
+# this is u.g.l.y. but lets force it for now to nuke selinux so the daemon starts
+if [ -f /usr/sbin/setenforce ]
+then
+    echo "disabling selinux - TEMPORARY DEVELOPMENT HACK ALERT"
+    run_command "sudo setenforce 0"
+fi
 
 echo "start weewx"
 run_command "sudo systemctl start weewx"
